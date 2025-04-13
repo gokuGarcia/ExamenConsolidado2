a416a6f46200840ed656160f8d94c90925312622
Esteban Garcia <este54191@gmail.com>
2025-04-13 01:13:18 -0500

    Implementación del CRUD para eventos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\OrganizadorController;
+use App\Http\Controllers\ParticipacionController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('eventos', EventoController::class);
+    Route::resource('organizadores', OrganizadorController::class);
+    Route::resource('participaciones', ParticipacionController::class);
 });
