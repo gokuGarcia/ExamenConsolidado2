0598ebe08c6046fe85d7a1d7620c01228a6b0aed
sebas444-Tv01 <sebasmusic1010@gmail.com>
2025-04-13 20:10:39 -0500

    COnfiguracion y modificacion del controler

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,69 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
-use App\Models\Eventos;
-
-class Controller extends BaseController
+abstract class Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function __construct()
-    {
-       $this->middleware('auth');
-    }
-    public function index()
-    {
-        $eventos= Eventos::all();
-        return view('eventos.index', compact('eventos'));
-    }
-    public function create()
-    {
-        return view('eventos.create');
-    }
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'Nombre' => 'required|string|max:255',
-            'Descripcion' => 'required|string|max:255',
-            'Fecha_Inicio' => 'required|date',
-            'Fecha_Fin' => 'required|date',
-            'Ubicacion' => 'required|string|max:255',
-        ]);
-
-        Eventos::create($validated);
-
-        return redirect()->route('eventos.index')->with('success', 'Evento creado exitosamente.');
-    }
-    public function show(Eventos $evento)
-    {
-        return view('eventos.show', compact('evento'));
-    }
-    public function edit(Eventos $evento)
-    {
-        return view('eventos.edit', compact('evento'));
-    }
-    public function update(Request $request, Eventos $evento)
-    {
-        $validated = $request->validate([
-            'Nombre' => 'required|string|max:255',
-            'Descripcion' => 'required|string|max:255',
-            'Fecha_Inicio' => 'required|date',
-            'Fecha_Fin' => 'required|date',
-            'Ubicacion' => 'required|string|max:255',
-        ]);
-
-       
-    }
-    public function destroy(Eventos $evento)
-    {
-        $evento->delete();
-
-        return redirect()->route('eventos.index')->with('success', 'Evento eliminado exitosamente.');
-    }
+    //
 }
-
