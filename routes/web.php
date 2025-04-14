771b643468166d1f6e69bc9ba38abcedc3606464
sebas444-Tv01 <sebasmusic1010@gmail.com>
2025-04-13 21:50:41 -0500

    Modificacion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\OrganizadorController;
+use App\Http\Controllers\ParticipacionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('eventos', EventoController::class);
+    Route::resource('organizadores', OrganizadorController::class);
+    Route::resource('participaciones', ParticipacionController::class);
 });
 
 require __DIR__.'/auth.php';
